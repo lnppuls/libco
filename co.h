4acbf9a1d9a7818380ac644f134e9f96591d2b1f
lnppuls <lnppuls@qq.com>
2024-05-12 13:12:36 +0000

    add README.md

--- co.h
+++ co.h
@@ -1,12 +1,11 @@
-
 typedef struct CO Co;
 
-// 创建线程返回线程信息的结构体
+// 创建协程
 Co * co_start(char *name, void (*func)(void *), void *arg);
 
 // 出让线程
 void co_yield();
 
-// 等待co线程执行完
+// 等待协程执行完
 void co_wait(Co *co);
 
